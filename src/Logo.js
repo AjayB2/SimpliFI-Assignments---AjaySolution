ad2171480da2e3eb205e39b068a5058975eb9854
AjayB2 <ajay932002@gmail.com>
2024-08-30 10:24:56 +0000

    solution

---
+++ src/Logo.js
@@ -0,0 +1,15 @@
+// src/Logo.js
+import React from 'react';
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <div className="circle"></div>
+      <div className="square"></div>
+      <h1 className="logo-text">SimpliFI</h1>
+    </div>
+  );
+};
+
+export default Logo;
